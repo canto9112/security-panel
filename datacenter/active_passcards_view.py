b349b7ec237b035a2ac7b982cf70bf035dd444a5
canto9112 <cantoweb112@gmail.com>
2021-04-08 19:00:49 +0300

    Высушил код

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    all_passcards = Passcard.objects.all()
-    active_passcars = all_passcards.filter(is_active=True)
+    active_passcars = Passcard.objects.filter(is_active=True)
 
     context = {
         "active_passcards": active_passcars,  # люди с активными пропусками
